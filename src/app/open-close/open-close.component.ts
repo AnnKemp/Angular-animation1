2a44efde8cb8a25fe5136a77ac53852b57cfb4c9
AnnKemp <ann.kemp@scarlet.be>
2021-01-28 16:44:37 +0100

    the first animation is working now!

--- src/app/open-close/open-close.component.ts
+++ src/app/open-close/open-close.component.ts
@@ -1,25 +1,25 @@
 import { Component } from '@angular/core';
-import { trigger, state, style, animate, transition} from '@angular/animations';
+import { trigger, state, style, animate, transition, AnimationEvent } from '@angular/animations';
 
 @Component({
   selector: 'app-open-close',
   animations: [
-    trigger('openClose', [ // dit werkt
+    trigger('openClose', [ 
       state('open', style({
         height: '200px',
         opacity: 1,
         backgroundColor: 'yellow'
       })),
-      state('closed', style({ // dit werkt
+      state('closed', style({ 
         height: '100px',
         opacity: 0.5,
         backgroundColor: 'green'
       })),
-      transition('open => closed', [ // de transities niet
-        animate('7s')
+      transition('open => closed', [ 
+        animate('1s')
       ]),
       transition('closed => open', [
-        animate('7s')
+        animate('0.5s')
       ]),
     ]),
   ],
