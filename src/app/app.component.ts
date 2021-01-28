79960eb23bed8c8ca0d2bb47ce2c1f3d374a1e61
AnnKemp <ann.kemp@scarlet.be>
2021-01-28 16:38:09 +0100

    basis animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,30 @@ import { trigger, state, style, animate, transition} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
+  animations: [
+    trigger('openClose', [
+      state('open', style({
+        height: '200px',
+        opacity: 1,
+        backgroundColor: 'yellow'
+      })),
+      state('closed', style({
+        height: '100px',
+        opacity: 0.5,
+        backgroundColor: 'green'
+      })),
+      transition('open => closed', [
+        animate('1s')
+      ]),
+      transition('closed => open', [
+        animate('0.5s')
+      ])
+    ])
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  animations: [
-    // animation triggers go here
-  ]
 })
+
 export class AppComponent {
-  title = 'exp-jquery-in-ang';
+   
 }
--- src/app/open-close/open-close.component.ts
+++ src/app/open-close/open-close.component.ts
@@ -1,42 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { trigger, state, style, animate, transition} from '@angular/animations';
 
 @Component({
   selector: 'app-open-close',
   animations: [
-    trigger('OpenClose', [
+    trigger('openClose', [ // dit werkt
       state('open', style({
         height: '200px',
         opacity: 1,
         backgroundColor: 'yellow'
       })),
-      state('closed', style({
-        height: '200px',
+      state('closed', style({ // dit werkt
+        height: '100px',
         opacity: 0.5,
         backgroundColor: 'green'
       })),
-      transition('open => closed', [
-        animate('1s')
+      transition('open => closed', [ // de transities niet
+        animate('7s')
       ]),
       transition('closed => open', [
-        animate('0.5s')
-      ])
-    ])
+        animate('7s')
+      ]),
+    ]),
   ],
   templateUrl: './open-close.component.html',
   styleUrls: ['./open-close.component.css'],
 })
-export class OpenCloseComponent implements OnInit {
+export class OpenCloseComponent{
   isOpen = true;
 
   toggle(){
-    this.isOpen = !this.isOpen;
+    this.isOpen = ! this.isOpen; // den toggle werkt ook niet, waar wordt die toggle functie aangeroepen?
   }
-  constructor() { }
-
- 
-  
-
-  ngOnInit(): void {
-  }
-
 }
